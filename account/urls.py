67a56b4d03dbeb27e63059f47c71f479d66eed1f
Kévin JAMET <keke.jamet@gmail.com>
2023-03-13 21:35:34 +0000

    v0.5.4
    finished project

--- account/urls.py
+++ account/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 
 from .views import SignUpView
 
 
 urlpatterns = [
+
+    path("password_reset/", auth_views.PasswordResetView.as_view(template_name='accounts/reset_password.html',
+            success_url=reverse_lazy('accounts:password_reset_done')), name='password_reset'),
+
     # Link to Signup Page
     # Pattern : url/signup
     path("signup/", SignUpView.as_view(), name="signup"),
